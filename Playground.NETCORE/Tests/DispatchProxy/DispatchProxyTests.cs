7c0a7831a0930253f97813db90832937edbf88a7
Nikos Polideropoulos <nickpolyder@hotmail.gr>
2018-01-21 20:47:43 +0200

    DispatchProxy

---
+++ Playground.NETCORE/Tests/DispatchProxy/DispatchProxyTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Playground.NETCORE.Tests.DispatchProxy
+{
+    public class DispatchProxyTests : ITestCase
+    {
+        /// <inheritdoc />
+        public bool Enabled { get; } = true;
+
+        /// <inheritdoc />
+        public string Name { get; } = "Dispatch Proxy Test";
+
+        /// <inheritdoc />
+        public void Run()
+        {
+            var dispatchInfo = new DispatchInformation
+            {
+                InformationType = "Dispatching Info's",
+                Title = "Heyaaaaaa",
+                Description = "Im trying to watch SPN"
+            };
+            var proxy = ExceptionDispatch<IDispatchInformation>.Create(dispatchInfo);
+            proxy.Display();
+            proxy.DisplayThrowException();
+
+            var objectWatch = System.Diagnostics.Stopwatch.StartNew();
+            dispatchInfo.Display();
+            objectWatch.Stop();
+
+            var proxyWatch = System.Diagnostics.Stopwatch.StartNew();
+            proxy.Display();
+            proxyWatch.Stop();
+
+            Console.WriteLine($"Object: {objectWatch.ElapsedMilliseconds} ms");
+            Console.WriteLine($"Proxy: {proxyWatch.ElapsedMilliseconds} ms");
+
+        }
+    }
+}
---
+++ Playground.NETCORE/Tests/DispatchProxy/ExceptionDispatch.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Reflection;
+
+namespace Playground.NETCORE.Tests.DispatchProxy
+{
+    public class ExceptionDispatch<TClass> : System.Reflection.DispatchProxy where TClass : class
+    {
+        private TClass _instance;
+
+        protected TClass Instance
+        {
+            get => _instance;
+
+            set => _instance = value;
+        }
+
+        public ExceptionDispatch()
+        { }
+
+        public ExceptionDispatch(TClass instance)
+        {
+            _instance = instance;
+        }
+
+        /// <inheritdoc />
+        protected override object Invoke(System.Reflection.MethodInfo targetMethod, object[] args)
+        {
+            Console.WriteLine($"Just got in {nameof(Invoke)} and just before run the method {targetMethod.Name}");
+            try
+            {
+                return targetMethod.Invoke(Instance, args);
+
+            }
+            catch (TargetInvocationException ex)
+            {
+                Console.WriteLine(ex.InnerException);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            return null;
+        }
+
+        public static TClass Create(TClass instance)
+        {
+            object proxy = Create<TClass, ExceptionDispatch<TClass>>();
+            ((ExceptionDispatch<TClass>)proxy).Instance = instance;
+            return (TClass)proxy;
+        }
+    }
+}
