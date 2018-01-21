7d00a57cfe5a36cec5de11ddb372c0380d38c474
Nikos Polideropoulos <nickpolyder@hotmail.gr>
2018-01-22 01:54:31 +0200

    sync context tests

---
+++ Playground.NETCORE/Tests/SyncContext/SyncContextTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Playground.NETCORE.Tests.SyncContext
+{
+    public class SyncContextTests : ITestCase
+    {
+        /// <inheritdoc />
+        public bool Enabled { get; } = true;
+
+        /// <inheritdoc />
+        public string Name { get; } = "Sync Context";
+
+        /// <inheritdoc />
+        public void Run()
+        {
+            Console.WriteLine($"My thread is: {Thread.CurrentThread.ManagedThreadId}.");
+
+            var syncContext = new SynchronizationContext();
+            syncContext.Post(post =>
+            {
+                Console.WriteLine($"My outer thread is: {Thread.CurrentThread.ManagedThreadId}. Yours is: {post}");
+            }, Thread.CurrentThread.ManagedThreadId);
+
+
+            Task.Run(async () =>
+            {
+                syncContext.Post(post =>
+                {
+                    Console.WriteLine($"My level 1 task thread is: {Thread.CurrentThread.ManagedThreadId}. Yours is: {post}");
+                }, Thread.CurrentThread.ManagedThreadId);
+
+
+                await Task.Run(() =>
+                {
+                    syncContext.Send(post =>
+                    {
+                        Console.WriteLine($"My level 2 task thread is: {Thread.CurrentThread.ManagedThreadId}. Yours is: {post}");
+                    }, Thread.CurrentThread.ManagedThreadId);
+
+                });
+            }).Wait();
+        }
+    }
+}
