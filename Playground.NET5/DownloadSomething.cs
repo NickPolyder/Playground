8a683059fe8b1d578862d67ce524ead6c5266702
Nick Polyderopoulos <nickpolyder@hotmail.gr>
2021-01-25 21:43:32 +0000

    Clean up

--- Playground.NET5/DownloadSomething.cs
+++ Playground.NET5/DownloadSomething.cs
@@ -1,4 +1,5 @@
-﻿using System.Net.Http;
+﻿using System;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace Playground.NET5
@@ -7,11 +8,31 @@ namespace Playground.NET5
 	{
 		public async Task DownloadFile(string fileName, string fileUrl)
 		{
-			using (var http = new HttpClient())
-			{
-				var file = await http.GetByteArrayAsync(fileUrl);
+			using var http = new HttpClient();
+			var file = await http.GetByteArrayAsync(fileUrl);
 
-				await System.IO.File.WriteAllBytesAsync($"C:\\temp\\{fileName}", file);
+			await System.IO.File.WriteAllBytesAsync($"C:\\temp\\{fileName}", file);
+		}
+
+		public async Task Run()
+		{
+			Console.Write("Get FileName: ");
+			var fileName = Console.ReadLine();
+
+			Console.Write("Get Url: ");
+			var url = Console.ReadLine();
+
+			try
+			{
+				await new DownloadSomething().DownloadFile(fileName, url);
+
+				Console.WriteLine($"C:\\temp\\{fileName}");
+			}
+			catch (Exception ex)
+			{
+				await Console.Error.WriteLineAsync(ex.Message);
+				await Console.Error.WriteLineAsync(ex.StackTrace);
+
 			}
 		}
 	}
--- Playground.NET5/Program.cs
+++ Playground.NET5/Program.cs
@@ -10,37 +10,6 @@ namespace Playground.NET5
 		{
 			Console.WriteLine("Hello World!");
 
-			//var exampleObj = new Example
-			//{
-			//	Id = 1,
-			//	Name = "Layla",
-			//	ExampleType =  ExampleTypeEnum.Three
-			//};
-
-			//var jsonText = System.Text.Json.JsonSerializer.Serialize(exampleObj);
-			//Console.WriteLine(jsonText);
-			
-			//Console.WriteLine(System.Text.Json.JsonSerializer.Deserialize<Example>(jsonText));
-
-			Console.Write("Get FileName: ");
-			var fileName = Console.ReadLine();
-
-			Console.Write("Get Url: ");
-			var url = Console.ReadLine();
-
-			try
-			{
-				await new DownloadSomething().DownloadFile(fileName, url);
-
-				Console.WriteLine($"C:\\temp\\{fileName}");
-			}
-			catch (Exception ex)
-			{
-				await Console.Error.WriteLineAsync(ex.Message);
-				await Console.Error.WriteLineAsync(ex.StackTrace);
-				
-			}
-
 			Console.ReadKey();
 		}
 	}
