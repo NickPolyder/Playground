c54c4d1ef67208f3a67226b2841e35a3a5b5d618
Nick Polyderopoulos <nickpolyder@hotmail.gr>
2024-09-15 14:36:39 +0100

    Console cursor manipulaiton

--- Console/Playground.NET5/Program.cs
+++ Console/Playground.NET5/Program.cs
@@ -14,8 +14,22 @@ namespace Playground.NET
 
             string? value = null;
 
-		   
-			Console.ReadKey();
+            
+            var position = Console.GetCursorPosition();
+            Console.WriteLine("Hello There");
+            var originalText = "Remaining Time: 35";
+
+            Console.WriteLine(originalText);
+            
+            await Task.Delay(TimeSpan.FromSeconds(5));
+			Console.SetCursorPosition(position.Left, position.Top);
+            Console.WriteLine("Who Are you ?");
+
+            var remainingTime = "Remaining Time: 5";
+            
+            Console.WriteLine(remainingTime + new string(' ', originalText.Length - (remainingTime.Length-1)));
+
+            Console.ReadKey();
 		}
 	}
 
